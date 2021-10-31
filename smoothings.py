2e07a1006f640a5c68ce3a4799042f1919e416ab
Konstantin Wrede <konstantin.wrede@turag.de>
2021-10-31 13:20:00 +0100

    changed PT1 smoothing to time discrete implementation

--- smoothings.py
+++ smoothings.py
@@ -1,10 +1,9 @@
-from scipy.signal import lti, lsim
+from scipy.signal import lfilter
 
 # Smoothing filters
 def pt1_smooth(tt, signal, cutoff_freq):
-    num_coeff = [1]
-    den_coeff = [1/cutoff_freq, 1]
-    trans_func = lti(num_coeff, den_coeff)
-    diff = lsim(trans_func, signal, tt)[1]
-    return diff
-    #test
+    T_sample = (tt[-1] - tt[0]) / (len(tt) - 1)
+    num_coeff = [T_sample * cutoff_freq]
+    den_coeff = [1, T_sample * cutoff_freq - 1]
+    smoothed = lfilter(num_coeff, den_coeff, signal)
+    return smoothed
