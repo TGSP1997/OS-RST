5e33735f6b645552d4d69a04ecc8473e52deba1b
Konstantin Wrede <konstantin.wrede@turag.de>
2021-11-05 16:04:01 +0100

    better plots

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ tt_step = 0.01
 tt = np.arange(0, tt_length * tt_step, tt_step)
 sine_period = 5
 true_sine = np.sin(2*np.pi/sine_period * tt)
+true_diff_sine = np.cos(2*np.pi/sine_period * tt)
 # generate white noise
 mean = 0
 std_dev = 0.05
@@ -30,7 +31,7 @@ kalman_smoothed = kalman_smooth(noisy_sine, std_dev, 30*std_dev)
 diff_kalman_smoothed = fwd_diff(tt, kalman_smoothed)
 
 # plot results
-plot_sig(tt, [noisy_sine, true_sine, diff_wiener_smoothed, diff_pt1_smoothed, diff_kalman_smoothed], ["noisy sine", "noiseless sine", "diff Wiener", "diff PT1", "diff Kalman"])
+plot_sig(tt, [true_diff_sine, diff_wiener_smoothed, diff_pt1_smoothed, diff_kalman_smoothed], ["true diff sine", "diff Wiener", "diff PT1", "diff Kalman"])
 plot_sig(tt, [diff_finite, diff_wiener_smoothed, diff_pt1_smoothed, diff_kalman_smoothed], ["diff unsmoothed", "diff Wiener", "diff PT1", "diff Kalman"])
-plot_sig(tt, [noisy_sine, pt1_smoothed, kalman_smoothed], ["noisy sine", "PT1 smoothed", "Kalman smoothed"])
+plot_sig(tt, [true_sine, noisy_sine, pt1_smoothed, wiener_smoothed, kalman_smoothed], ["true sine", "noisy sine", "PT1 smoothed", "Wiener smoothed", "Kalman smoothed"])
 plt.show()
