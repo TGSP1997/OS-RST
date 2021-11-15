5268d5f78b794ff23b338bfc3abdd0f9ea780e6d
Konstantin Wrede <konstantin.wrede@turag.de>
2021-11-15 10:37:42 +0100

    std_dev for noise estimation in Wiener, double integrator setup in Kalman

--- filters.py
+++ filters.py
@@ -12,43 +12,70 @@ def pt1_smooth(tt, signal, cutoff_freq):
     return smoothed
 
 # noncausal FIR Wiener filter
-def wiener_smooth(signal, noise, filter_order, noise_stdev):
-    S_nn = np.square(np.abs(np.fft.fft(noise)/len(noise)))
+def wiener_smooth(signal, noise_stdev):
+    S_nn = noise_stdev**2*np.ones(len(signal))
     S_yy = np.square(np.abs(np.fft.fft(signal)/len(signal)))
     H_noncausal = np.maximum(0, np.divide(S_yy - S_nn , S_yy))
     X_hat = np.multiply(H_noncausal, np.fft.fft(signal))
     x_hat = np.real(np.fft.ifft(X_hat))
     return x_hat
 
-# https://scipy-cookbook.readthedocs.io/items/KalmanFiltering.html
-def kalman_smooth(signal, noise_stdev, measure_stdev):
-    n_iter = len(signal)
-    sz = (n_iter,) # size of array
-    Q = noise_stdev**2 # process variance
-    R = measure_stdev**2 # estimate of measurement variance, change to see effect
+# https://github.com/zziz/kalman-filter
+class KalmanFilter(object):
+    def __init__(self, F = None, B = None, H = None, Q = None, R = None, P = None, x0 = None):
 
-    # allocate space for arrays
-    xhat=np.zeros(sz)      # a posteri estimate of x
-    P=np.zeros(sz)         # a posteri error estimate
-    xhatminus=np.zeros(sz) # a priori estimate of x
-    Pminus=np.zeros(sz)    # a priori error estimate
-    K=np.zeros(sz)         # gain or blending factor
+        if(F is None or H is None):
+            raise ValueError("Set proper system dynamics.")
 
-    # intial guesses
-    xhat[0] = 0.0
-    P[0] = 1.0
+        self.n = F.shape[1]
+        self.m = H.shape[1]
 
-    for k in range(1,n_iter):
-        # time update
-        xhatminus[k] = xhat[k-1]
-        Pminus[k] = P[k-1]+Q
+        self.F = F
+        self.H = H
+        self.B = 0 if B is None else B
+        self.Q = np.eye(self.n) if Q is None else Q
+        self.R = np.eye(self.n) if R is None else R
+        self.P = np.eye(self.n) if P is None else P
+        self.x = np.zeros((self.n, 1)) if x0 is None else x0
 
-        # measurement update
-        K[k] = Pminus[k]/( Pminus[k]+R )
-        xhat[k] = xhatminus[k]+K[k]*(signal[k]-xhatminus[k])
-        P[k] = (1-K[k])*Pminus[k]
+    # prediction step
+    def predict(self, u = 0):
+        self.x = np.dot(self.F, self.x) + np.dot(self.B, u)
+        self.P = np.dot(np.dot(self.F, self.P), self.F.T) + self.Q
+        return self.x
 
-    return xhat
+    # correction / update step
+    def update(self, z):
+        y = z - np.dot(self.H, self.x)
+        S = self.R + np.dot(self.H, np.dot(self.P, self.H.T))
+        K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))
+        self.x = self.x + np.dot(K, y)
+        I = np.eye(self.n)
+        self.P = np.dot(np.dot(I - np.dot(K, self.H), self.P), 
+        	(I - np.dot(K, self.H)).T) + np.dot(np.dot(K, self.R), K.T)
+
+def kalman_smooth(tt, signal, noise_stdev, process_stdev):
+    T_sample = (tt[-1] - tt[0]) / (len(tt) - 1)
+    F = np.array([[1, T_sample], [0, 1]])
+    B = np.array([0, 0]).reshape(2, 1)#np.array([T_sample**2/2, T_sample]).reshape(2, 1)
+    H = np.array([[1, 0]]).reshape(1, 2)
+    Q = np.array([[process_stdev, 0], [0, process_stdev]])
+    R = np.array([noise_stdev**2]).reshape(1, 1)
+
+    measurements = signal
+    kf = KalmanFilter(F = F, B = B, H = H, Q = Q, R = R)
+    predictions = []
+    x_1 = []
+    x_2 = []
+
+    for z in measurements:
+        x_hat = kf.predict()
+        predictions.append(np.dot(H, x_hat))
+        x_1.append(x_hat[0])
+        x_2.append(x_hat[1])
+        kf.update(z)
+    
+    return [x_1, x_2]
 
 def savgol_smooth(signal,window_length,polyorder,sample_frequency,pos=None):
     #general savgol filtering in time domain
