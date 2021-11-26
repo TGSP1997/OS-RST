fixed wrong derivative on sine, removed DC part of input in main

input_function.py
@@ -35,7 +35,7 @@ class Input_Function:
     def __get_fun_sine(self):
         t = np.arange(0, self.point_counter * self.sampling_period, self.sampling_period)
         n = self.parameters[0]*np.sin(2*np.pi/self.parameters[1] * t + self.parameters[2]) + self.parameters[3]
-        n_dot = np.cos(2*np.pi/self.parameters[1] * t)
+        n_dot = 2*np.pi/self.parameters[1]*self.parameters[0]*np.cos(2*np.pi/self.parameters[1] * t)
         return t, n, n_dot
 
         

main.py
@@ -11,7 +11,7 @@ noise_std_dev   = 0.1
 alpha           = 0.4
 
 
-sine    = Input_Function(Input_Enum.SINE, [1, 0.1, 0, 0.5])
+sine    = Input_Function(Input_Enum.SINE, [1, 0.1, 0, 0])
 white   = Noise(Noise_Enum.WHITE, noise_std_dev)
 pt1     = Filter(Filter_Enum.PT1, 1e2)
 wiener  = Filter(Filter_Enum.WIENER, noise_std_dev)
@@ -32,7 +32,7 @@ def filter_cost(para_in, t, y, true_y, filter, cost):
 
 
 def kalman_filter_cost(para_in, t, y, para_filt, true_y, filter, cost):
-        y_hat_kalman = filter.filter_fun(t, y, para = para_filt)[1]
+        y_hat_kalman = filter.filter_fun(t, y, para = para_filt)[0]
         return cost.cost(y_hat_kalman, true_y)
 
 f_min = minimize(filter_cost,0.1,args=(time, y, true_sine, pt1,cost))
@@ -61,6 +61,7 @@ y_hat_dot_wiener = fwd_diff(time, y_hat_wiener)
 
 # Kalman-smoothing
 y_kalman_true = -10*(time-0.5)**2 + 2.5
+y_dot_kalman_true = -20*(time-0.5)
 y_kalman = white.apply_noise(y_kalman_true)
 kalman_process_noise = minimize(kalman_filter_cost, 3e6, args=(time, y_kalman, [np.array([0, 11]).reshape(2, 1), noise_std_dev, 3e6], y_kalman_true, kalman, cost))
 kalman_process_noise = kalman_process_noise.x
@@ -81,14 +82,9 @@ print(f"Mean Squared Error of Differentials: \n \
         Brown-Holt: {cost.cost(y_hat_dot_brown, true_sine_dot)} \n" )
 
 # plot results
-plot.plot_sig(time, [true_sine_dot, y_hat_dot_wiener, y_hat_dot_pt1, y_hat_dot_kalman, y_hat_dot_brown], ["true diff sine", "diff Wiener", "diff PT1", "diff Kalman", "diff Brown"])
-plot.plot_sig(time, [diff_finite, y_hat_dot_wiener, y_hat_dot_pt1, y_hat_dot_kalman, y_hat_dot_brown], ["diff unsmoothed", "diff Wiener", "diff PT1", "diff Kalman", "diff Brown"])
-plot.plot_sig(time, [true_sine, y, y_hat_pt1, y_hat_wiener, y_hat_kalman, y_hat_brown], ["true sine", "noisy sine", "PT1 smoothed", "Wiener smoothed", "Kalman smoothed", "Brown smoothed"])
-
-# plot_sub.plot_sig(time,[true_sine_dot, y_hat_dot_wiener, y_hat_dot_pt1, y_hat_dot_kalman, y_hat_dot_brown], ["true diff sine", "diff Wiener", "diff PT1", "diff Kalman", "diff Brown"])
-# plot_sub.plot_sig(time,[true_sine, y_hat_wiener, y_hat_pt1, y_hat_kalman, y_hat_brown], ["true diff sine", "diff Wiener", "diff PT1", "diff Kalman", "diff Brown"])
-# 
-
+plot.plot_sig(time, [true_sine_dot, y_hat_dot_wiener], ["true diff sine", "diff Wiener"])
+plot.plot_sig(time, [diff_finite, y_hat_dot_wiener], ["diff unsmoothed", "diff Wiener"])
+plot.plot_sig(time, [true_sine, y, y_hat_wiener], ["true sine", "noisy sine", "Wiener smoothed"])
 plot.plot_sig(time, [y_kalman, y_hat_kalman, y_hat_dot_kalman], ["true", "Kalman", "dot Kalman"])
 plt.show()
 