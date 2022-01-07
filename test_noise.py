9d27d4b06b3cc0757f1da9d73581029f3d01c82e
Konstantin Wrede <konstantin.wrede@turag.de>
2022-01-07 10:54:14 +0100

    spectral power density and noise/metric checks

--- test_noise.py
+++ test_noise.py
@@ -1,5 +1,6 @@
 from cost import Cost_Enum
 from scipy.optimize import minimize
+from scipy import signal
 
 from input_function import *
 from noises import *
@@ -7,17 +8,40 @@ from filter import *
 from plot_sig import *
 from cost import *
 
-noise_std_dev = 0.1
-
-sine    = Input_Function(Input_Enum.SINE, [1, 0.1, 0, 0.5])
-white   = Noise(Noise_Enum.WHITE, noise_std_dev)
-pink   = Noise(Noise_Enum.PINK, noise_std_dev)
-brown   = Noise(Noise_Enum.BROWN, noise_std_dev)
+noise_std_dev = 0.01
+sine    = Input_Function(Input_Enum.SINE, [1, 1, 0, 0], point_counter=1e3)
+polynom = Input_Function(Input_Enum.POLYNOM, [1,1,1,1]) #coefs in descending order 2x^2+1 = [2,0,1]
+exp     = Input_Function(Input_Enum.EXP, [1,2,0,0]) #coefs [a,b,c,d]= a*e^(t/b+c)+d
+whites  = [Noise(Noise_Enum.WHITE, noise_std_dev, seed=i) for i in range(10)]
+pinks   = [Noise(Noise_Enum.PINK, noise_std_dev, seed=i) for i in range(10)]
+browns   = [Noise(Noise_Enum.BROWN, noise_std_dev, seed=i) for i in range(10)]
 quant   = Noise(Noise_Enum.QUANT, 0.5)
 plot    = Plot_Sig(Plot_Enum.MULTI, "Overview", [])
 
-time, true_sine, true_sine_dot = sine.get_fun()
-y = quant.apply_noise(true_sine)
+cost    = Cost(Cost_Enum.PHASE_SHIFT)
 
-plot.plot_sig(time, [true_sine, y],["Roh","Rausch"])
+time, true_sine, true_sine_dot = sine.get_fun()
+norm_freq = time[:round(len(time))] / (time[-1] - time[0])
+
+print(cost.cost(true_sine_dot, true_sine))
+
+y = browns[0].apply_noise(true_sine)
+y2 = pinks[0].apply_noise(true_sine)
+
+### plot power spectral density
+'''
+freqs, psd = signal.welch(y)
+freqs, psd2 = signal.welch(y2)
+psd = 20*np.log10(psd)
+psd2 = 20*np.log10(psd2)
+
+plt.figure(figsize=(5, 4))
+plt.semilogx(freqs, psd, freqs, psd2)
+plt.title('PSD: power spectral density')
+plt.xlabel('Frequency')
+plt.ylabel('dB')
+plt.tight_layout()
+'''
+
+plot.plot_sig(time, [true_sine, true_sine_dot],["Roh","Rausch"])
 plt.show()
