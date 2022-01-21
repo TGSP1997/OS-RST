dca6e2a0934e567e310a7645349775a87e8e3435
Arktarus <67394197+Arktarus@users.noreply.github.com>
2022-01-21 09:50:03 +0100

    Updated Butter calculation

--- main_jonas_butterworth.py
+++ main_jonas_butterworth.py
@@ -13,82 +13,294 @@ from plot_sig import *
 from cost import *
 
 
+step_size       = 2e-3
+point_counter   = int(1/step_size)
+
 noise_std_dev   = 0.1
+
+order           = 4
 freq            = 10 / (1000 / 2) # Normalisierte Grenzfrequenz mit w = fc / (fs / 2)
 
-bounds          = ((0.0001, 0.9999),) # Komische Tupeldarstellung damit Minimize Glücklich ist.
+bounds          = ((0.001, 0.9),) # Komische Tupeldarstellung damit Minimize Glücklich ist.
 
 # 1. Filtereigenschaften auf Sinus
-for order in range(1,5):
-        sine    = Input_Function(Input_Enum.SINE, [1, 0.4, 0, 0])
+sine    = Input_Function(Input_Enum.SINE, [1, 0.5, 0, 0])
 
-        butter   = Filter(Filter_Enum.BUTTERWORTH, [order,freq])
-        
-        white   = Noise(Noise_Enum.WHITE, noise_std_dev)
-        brown   = Noise(Noise_Enum.BROWN, noise_std_dev)
-        quant   = Noise(Noise_Enum.QUANT, noise_std_dev)
+butter   = Filter(Filter_Enum.BUTTERWORTH, [order,freq])
 
-        cost    = Cost(Cost_Enum.MSE)
+white   = Noise(Noise_Enum.WHITE, noise_std_dev)
+brown   = Noise(Noise_Enum.BROWN, noise_std_dev)
+quant   = Noise(Noise_Enum.QUANT, noise_std_dev)
 
-        plot  = Plot_Sig(Plot_Enum.FILTER1, "Filterung",[])
+cost    = Cost(Cost_Enum.MSE)
 
-        def filter_cost(para_in, t, y, x, filter, cost):
-                y_hat = filter.filter_fun(t, y, para = [filter.parameters[0], para_in])
-                return cost.cost(y_hat, x)
-        def filter_cost_diff(para_in, t, y, x_dot, filter, cost):
-                y_hat_dot = filter.filter_diff(t, y, para = [filter.parameters[0], para_in])
-                return cost.cost(y_hat_dot, x_dot)
+plot1  = Plot_Sig(Plot_Enum.FILTER1, "Filterung",[])
 
-        t, x, x_dot = sine.get_fun()
-        y_white = white.apply_noise(x)
-        y_brown = brown.apply_noise(x)
-        y_quant = quant.apply_noise(x)
+def filter_cost(para_in, t, y, x, filter, cost):
+        y_hat = filter.filter_fun(t, y, para = [filter.parameters[0], para_in])
+        return cost.cost(y_hat, x) 
+def filter_cost_diff(para_in, t, y, x_dot, filter, cost):
+        y_hat_dot = filter.filter_diff(t, y, para = [filter.parameters[0], para_in])
+        return cost.cost(y_hat_dot, x_dot)
 
-        freq_min_white = minimize(filter_cost,freq,args=(t, y_white, x, butter ,cost), bounds= bounds)
-        x_hat_min_white = butter.filter_fun(t,y_white,para = [butter.parameters[0],freq_min_white.x])
-        cost_white = cost.cost(x_hat_min_white,x)
-        standard_cost_white = cost.cost(y_white,x)
+t, x, x_dot = sine.get_fun()
+y_white = white.apply_noise(x)
+y_brown = brown.apply_noise(x)
+y_quant = quant.apply_noise(x)
 
-        freq_min_brown = minimize(filter_cost,freq,args=(t, y_brown, x, butter ,cost), bounds= bounds)
-        x_hat_min_brown = butter.filter_fun(t,y_brown,para = [butter.parameters[0],freq_min_brown.x])
-        cost_brown = cost.cost(x_hat_min_brown,x)
-        standard_cost_brown = cost.cost(y_brown,x)
+freq_min_white = minimize(filter_cost,freq,args=(t, y_white, x, butter ,cost), bounds= bounds)
+x_hat_min_white = butter.filter_fun(t,y_white,para = [butter.parameters[0],freq_min_white.x])
+cost_white = cost.cost(x_hat_min_white,x)
+standard_cost_white = cost.cost(y_white,x)
 
-        freq_min_quant = minimize(filter_cost,freq,args=(t, y_quant, x, butter ,cost), bounds= bounds)
-        x_hat_min_quant = butter.filter_fun(t,y_quant,para = [butter.parameters[0],freq_min_quant.x])
-        cost_quant = cost.cost(x_hat_min_quant,x)
-        standard_cost_quant = cost.cost(y_quant,x)
+freq_min_brown = minimize(filter_cost,freq,args=(t, y_brown, x, butter ,cost), bounds= bounds)
+x_hat_min_brown = butter.filter_fun(t,y_brown,para = [butter.parameters[0],freq_min_brown.x])
+cost_brown = cost.cost(x_hat_min_brown,x)
+standard_cost_brown = cost.cost(y_brown,x)
 
-        box_label_white = '\n'.join((
-                r'White Noise',
-                r'$\sigma_{Noise}=%.2f$' % (noise_std_dev, ),
-                r'$f=%.2f$' % (freq_min_white.x, ),
-                r'$MSE_{Noise}=%.5f$' % (standard_cost_white, ),
-                r'$MSE_{Filter}=%.5f$' % (cost_white, ),
-                r'$r_{MSE}=%.2f$ %%' % (100*cost_white/standard_cost_white, )))
+freq_min_quant = minimize(filter_cost,freq,args=(t, y_quant, x, butter ,cost), bounds= bounds)
+x_hat_min_quant = butter.filter_fun(t,y_quant,para = [butter.parameters[0],freq_min_quant.x])
+cost_quant = cost.cost(x_hat_min_quant,x)
+standard_cost_quant = cost.cost(y_quant,x)
 
-        box_label_brown = '\n'.join((
-                r'Brown Noise',
-                r'$\sigma_{Noise}=%.2f$' % (noise_std_dev, ),
-                r'$f=%.2f$' % (freq_min_brown.x, ),
-                r'$MSE_{Noise}=%.5f$' % (standard_cost_brown, ),
-                r'$MSE_{Filter}=%.5f$' % (cost_brown, ),
-                r'$r_{MSE}=%.2f$ %%' % (100*cost_brown/standard_cost_brown, )))
+box_label_white = '\n'.join((
+        r'White Noise',
+        r'$\sigma_{Noise}=%.2f$' % (noise_std_dev, ),
+        r'$f=%.2f$' % (freq_min_white.x, ),
+        r'$MSE_{Noise}=%.5f$' % (standard_cost_white, ),
+        r'$MSE_{Filter}=%.5f$' % (cost_white, ),
+        r'$r_{MSE}=%.2f$ %%' % (100*cost_white/standard_cost_white, )))
 
-        box_label_quant = '\n'.join((
-                r'Quantisation Noise',
-                r'$stepsize=%.2f$' % (noise_std_dev, ),
-                r'$f=%.2f$' % (freq_min_quant.x, ),
-                r'$MSE_{Noise}=%.5f$' % (standard_cost_quant, ),
-                r'$MSE_{Filter}=%.5f$' % (cost_quant, ),
-                r'$r_{MSE}=%.2f$ %%' % (100*cost_quant/standard_cost_quant, )))
+box_label_brown = '\n'.join((
+        r'Brown Noise',
+        r'$\sigma_{Noise}=%.2f$' % (noise_std_dev, ),
+        r'$f=%.2f$' % (freq_min_brown.x, ),
+        r'$MSE_{Noise}=%.5f$' % (standard_cost_brown, ),
+        r'$MSE_{Filter}=%.5f$' % (cost_brown, ),
+        r'$r_{MSE}=%.2f$ %%' % (100*cost_brown/standard_cost_brown, )))
 
-        plot.plot_sig(t,[x,y_white,y_brown,y_quant,x_hat_min_white,x_hat_min_brown,x_hat_min_quant],['Input Signal',
-        'Signal with White Noise',
-        'Signal with Brown Noise',
-        'Signal with Quantisation Noise',
-        'Butterworth Filter order: '+ str(order) +' (White Noise)',
-        'Butterworth Filter order: '+ str(order) +' (Brown Noise)',
-        'Butterworth Filter order: '+ str(order) +' (Quantisation)',
-        box_label_white,box_label_brown,box_label_quant],True)
+box_label_quant = '\n'.join((
+        r'Quantisation Noise',
+        r'$stepsize=%.2f$' % (noise_std_dev, ),
+        r'$f=%.2f$' % (freq_min_quant.x, ),
+        r'$MSE_{Noise}=%.5f$' % (standard_cost_quant, ),
+        r'$MSE_{Filter}=%.5f$' % (cost_quant, ),
+        r'$r_{MSE}=%.2f$ %%' % (100*cost_quant/standard_cost_quant, )))
+
+plot1.plot_sig(t,[x,y_white,y_brown,y_quant,x_hat_min_white,x_hat_min_brown,x_hat_min_quant],['Input Signal',
+'Signal with White Noise',
+'Signal with Brown Noise',
+'Signal with Quantisation Noise',
+'Butterworth Filter order: '+ str(order) +'. order',
+'Butterworth Filter order: '+ str(order) +'. order',
+'Butterworth Filter order: '+ str(order) +'. order',
+box_label_white,box_label_brown,box_label_quant],True)
+
+
+# 2. Ableitungseigenschaften auf Sinus
+
+y_white_dot = np.diff(y_white, append = 0)/step_size
+y_brown_dot = np.diff(y_brown, append = 0)/step_size
+y_quant_dot = np.diff(y_quant, append = 0)/step_size
+
+plot2  = Plot_Sig(Plot_Enum.FILTER2, "Filterung",[])
+
+freq_min_white = minimize(filter_cost_diff,freq,args=(t, y_white, x_dot, butter ,cost), bounds = bounds)
+x_hat_min_white = butter.filter_diff(t,y_white,para = [butter.parameters[0],freq_min_white.x]) 
+cost_white = cost.cost(x_hat_min_white,x_dot)
+standard_cost_white = cost.cost(y_white_dot,x_dot)
+
+freq_min_brown = minimize(filter_cost_diff,freq,args=(t, y_brown, x_dot, butter ,cost), bounds = bounds)
+x_hat_min_brown = butter.filter_diff(t,y_brown,para = [butter.parameters[0],freq_min_white.x]) 
+cost_brown = cost.cost(x_hat_min_brown,x_dot)
+standard_cost_brown = cost.cost(y_brown_dot,x_dot)
+
+freq_min_quant = minimize(filter_cost_diff,freq,args=(t, y_quant, x_dot, butter ,cost), bounds = bounds)
+x_hat_min_quant = butter.filter_diff(t,y_quant,para = [butter.parameters[0],freq_min_white.x])  
+cost_quant = cost.cost(x_hat_min_quant,x_dot)
+standard_cost_quant = cost.cost(y_quant_dot,x_dot)
+
+box_label_white = '\n'.join((
+        r'White Noise',
+        r'$\sigma_{Noise}=%.2f$' % (noise_std_dev, ),
+        r'$f=%.3f$' % (freq_min_white.x, ),
+        r'$MSE_{Noise}=%.2f$' % (standard_cost_white, ),
+        r'$MSE_{Filter}=%.2f$' % (cost_white, ),
+        r'$r_{MSE}=%.2f$ %%' % (100*cost_white/standard_cost_white, )))
+
+box_label_brown = '\n'.join((
+        r'Brown Noise',
+        r'$\sigma_{Noise}=%.2f$' % (noise_std_dev, ),
+        r'$f=%.3f$' % (freq_min_brown.x, ),
+        r'$MSE_{Noise}=%.2f$' % (standard_cost_brown, ),
+        r'$MSE_{Filter}=%.2f$' % (cost_brown, ),
+        r'$r_{MSE}=%.2f$ %%' % (100*cost_brown/standard_cost_brown, )))
+
+box_label_quant = '\n'.join((
+        r'Quantisation Noise',
+        r'$stepsize=%.2f$' % (noise_std_dev, ),
+        r'$f=%.3f$' % (freq_min_quant.x, ),
+        r'$MSE_{Noise}=%.2f$' % (standard_cost_quant, ),
+        r'$MSE_{Filter}=%.2f$' % (cost_quant, ),
+        r'$r_{MSE}=%.2f$ %%' % (100*cost_quant/standard_cost_quant, )))
+
+plot2.plot_sig(t,[x_dot,y_white_dot,y_brown_dot,y_quant_dot,x_hat_min_white,x_hat_min_brown,x_hat_min_quant],['Input Signal',
+'Diff of signal with White Noise',
+'Diff of signal with Brown Noise',
+'Diff of signal with Quantisation Noise',
+'Butterworth Filter '+ str(order) +'. order and Differentation',
+'Butterworth Filter '+ str(order) +'. order and Differentation',
+'Butterworth Filter '+ str(order) +'. order and Differentation',
+box_label_white,box_label_brown,box_label_quant],True)
+
+# 3. Filtereigenschaften auf Polynom
+
+poly    = Input_Function(Input_Enum.POLYNOM, [100,-150,50,0], sampling_period = step_size, point_counter = point_counter)
+
+white   = Noise(Noise_Enum.WHITE, 5*noise_std_dev)
+brown   = Noise(Noise_Enum.BROWN, 5*noise_std_dev)
+quant   = Noise(Noise_Enum.QUANT, 5*noise_std_dev)
+
+t, x, x_dot = poly.get_fun()
+y_white = white.apply_noise(x)
+y_brown = brown.apply_noise(x)
+y_quant = quant.apply_noise(x)
+
+freq_min_white = minimize(filter_cost,freq,args=(t, y_white, x, butter, cost), bounds = bounds)
+x_hat_min_white = butter.filter_fun(t,y_white,para = [butter.parameters[0], freq_min_white.x])
+cost_white = cost.cost(x_hat_min_white,x)
+standard_cost_white = cost.cost(y_white,x)
+
+freq_min_brown = minimize(filter_cost,freq,args=(t, y_brown, x, butter, cost), bounds = bounds)
+x_hat_min_brown = butter.filter_fun(t,y_brown,para = [butter.parameters[0], freq_min_brown.x])
+cost_brown = cost.cost(x_hat_min_brown,x)
+standard_cost_brown = cost.cost(y_brown,x)
+
+freq_min_quant = minimize(filter_cost,freq,args=(t, y_quant, x, butter, cost), bounds = bounds)
+x_hat_min_quant = butter.filter_fun(t,y_quant,para = [butter.parameters[0], freq_min_quant.x])
+cost_quant = cost.cost(x_hat_min_quant,x)
+standard_cost_quant = cost.cost(y_quant,x)
+
+box_label_white = '\n'.join((
+        r'White Noise',
+        r'$\sigma_{Noise}=%.2f$' % (noise_std_dev, ),
+        r'$f=%.2f$' % (freq_min_white.x, ),
+        r'$MSE_{Noise}=%.5f$' % (standard_cost_white, ),
+        r'$MSE_{Filter}=%.5f$' % (cost_white, ),
+        r'$r_{MSE}=%.2f$ %%' % (100*cost_white/standard_cost_white, )))
+
+box_label_brown = '\n'.join((
+        r'Brown Noise',
+        r'$\sigma_{Noise}=%.2f$' % (noise_std_dev, ),
+        r'$f=%.2f$' % (freq_min_brown.x, ),
+        r'$MSE_{Noise}=%.5f$' % (standard_cost_brown, ),
+        r'$MSE_{Filter}=%.5f$' % (cost_brown, ),
+        r'$r_{MSE}=%.2f$ %%' % (100*cost_brown/standard_cost_brown, )))
+
+box_label_quant = '\n'.join((
+        r'Quantisation Noise',
+        r'$stepsize=%.2f$' % (noise_std_dev, ),
+        r'$f=%.2f$' % (freq_min_quant.x, ),
+        r'$MSE_{Noise}=%.5f$' % (standard_cost_quant, ),
+        r'$MSE_{Filter}=%.5f$' % (cost_quant, ),
+        r'$r_{MSE}=%.2f$ %%' % (100*cost_quant/standard_cost_quant, )))
+
+plot1.plot_sig(t,[x,y_white,y_brown,y_quant,x_hat_min_white,x_hat_min_brown,x_hat_min_quant],['Input Signal',
+'Signal with White Noise',
+'Signal with Brown Noise',
+'Signal with Quantisation Noise',
+'Butterworth Filter'+ str(order),
+'Butterworth Filter'+ str(order),
+'Butterworth Filter'+ str(order),
+box_label_white,box_label_brown,box_label_quant],True)
+
+# 4. Ableitungseigenschaften auf Polynom
+
+y_white_dot = np.diff(y_white, append = 0)/step_size
+y_brown_dot = np.diff(y_brown, append = 0)/step_size
+y_quant_dot = np.diff(y_quant, append = 0)/step_size
+
+freq_min_white = minimize(filter_cost_diff, freq, args=(t, y_white, x_dot, butter ,cost), bounds = bounds)
+x_hat_min_white = butter.filter_diff(t,y_white,para = [butter.parameters[0], freq_min_white.x])
+cost_white = cost.cost(x_hat_min_white,x_dot)
+standard_cost_white = cost.cost(y_white_dot,x_dot)
+
+freq_min_brown = minimize(filter_cost_diff, freq, args=(t, y_brown, x_dot, butter ,cost), bounds = bounds)
+x_hat_min_brown = butter.filter_diff(t,y_brown,para = [butter.parameters[0], freq_min_white.x])
+cost_brown = cost.cost(x_hat_min_brown,x_dot)
+standard_cost_brown = cost.cost(y_brown_dot,x_dot)
+
+freq_min_quant = minimize(filter_cost_diff, freq, args=(t, y_quant, x_dot, butter ,cost), bounds = bounds)
+x_hat_min_quant = butter.filter_diff(t,y_quant,para = [butter.parameters[0], freq_min_white.x])
+cost_quant = cost.cost(x_hat_min_quant,x_dot)
+standard_cost_quant = cost.cost(y_quant_dot,x_dot)
+
+box_label_white = '\n'.join((
+        r'White Noise',
+        r'$\sigma_{Noise}=%.2f$' % (noise_std_dev, ),
+        r'$f=%.3f$' % (freq_min_white.x[0], ),
+        r'$MSE_{Noise}=%.2f$' % (standard_cost_white, ),
+        r'$MSE_{Filter}=%.2f$' % (cost_white, ),
+        r'$r_{MSE}=%.2f$ %%' % (100*cost_white/standard_cost_white, )))
+
+box_label_brown = '\n'.join((
+        r'Brown Noise',
+        r'$\sigma_{Noise}=%.2f$' % (noise_std_dev, ),
+        r'$f=%.3f$' % (freq_min_brown.x[0], ),
+        r'$MSE_{Noise}=%.2f$' % (standard_cost_brown, ),
+        r'$MSE_{Filter}=%.2f$' % (cost_brown, ),
+        r'$r_{MSE}=%.2f$ %%' % (100*cost_brown/standard_cost_brown, )))
+
+box_label_quant = '\n'.join((
+        r'Quantisation Noise',
+        r'$stepsize=%.2f$' % (noise_std_dev, ),
+        r'$f=%.3f$' % (freq_min_quant.x[0], ),
+        r'$MSE_{Noise}=%.2f$' % (standard_cost_quant, ),
+        r'$MSE_{Filter}=%.2f$' % (cost_quant, ),
+        r'$r_{MSE}=%.2f$ %%' % (100*cost_quant/standard_cost_quant, )))
+
+plot2.plot_sig(t,[x_dot,y_white_dot,y_brown_dot,y_quant_dot,x_hat_min_white,x_hat_min_brown,x_hat_min_quant],['Input Signal',
+'Diff of signal with White Noise',
+'Diff of signal with Brown Noise',
+'Diff of signal with Quantisation Noise',
+'Butterworth Filter '+ str(order) +'. order and Differentation',
+'Butterworth Filter '+ str(order) +'. order and Differentation',
+'Butterworth Filter '+ str(order) +'. order and Differentation',
+box_label_white,box_label_brown,box_label_quant],True)
+
+# 10. Bode-Plot
+
+# Übertragungsfunktion des Filters bestimmen
+
+butter1   = Filter(Filter_Enum.BUTTERWORTH, [1,freq])
+butter2   = Filter(Filter_Enum.BUTTERWORTH, [2,freq])
+butter3   = Filter(Filter_Enum.BUTTERWORTH, [3,freq])
+butter4   = Filter(Filter_Enum.BUTTERWORTH, [4,freq])
+butter5   = Filter(Filter_Enum.BUTTERWORTH, [5,freq])
+
+u = np.zeros(int(point_counter))
+u[10] = 1
+t = np.linspace(0,1,num = int(point_counter))
+
+o1      = butter1.filter_fun(t,u)
+o2      = butter2.filter_fun(t,u)
+o3      = butter3.filter_fun(t,u)
+o4      = butter4.filter_fun(t,u)
+o5      = butter5.filter_fun(t,u)
+
+
+plot_bode = Plot_Sig(Plot_Enum.BODE,"Bode Plot",parameters = 0)
+
+plot_bode.plot_sig(t,[[u,u,u,u,u],[o1,o2,o3,o4,o5]],[
+        "1st order", 
+        "2nd order",
+        "3rd order",
+        "4th order",
+        "5th order",])
+
+
+
+
+
+
 plt.show()
