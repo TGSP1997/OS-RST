84cf7df7958e01ed428e7bfcd4866aacf63b456f
TGSP1997 <Gymisandro@web.de>
2022-01-22 21:34:51 +0100

    unwrap

--- plot_sig.py
+++ plot_sig.py
@@ -272,7 +272,7 @@ class Plot_Sig:
             if max(abs(np.imag(G_fft))) < 1e-10:
                 G_fft = np.real(G_fft)
             ax_amp.semilogx(norm_freq,20*np.log10(abs(G_fft[:round(len(G_fft)/2)])), label=labels[i])
-            ax_phase.semilogx(norm_freq,np.multiply(np.angle(G_fft[:round(len(G_fft)/2)]),(180/np.pi)), label=labels[i])
+            ax_phase.semilogx(norm_freq,np.multiply(np.unwrap(np.angle(G_fft[:round(len(G_fft)/2)])),(180/np.pi)), label=labels[i])
         
         plt.sca(ax_amp)    
         plt.ylabel('Amplitude [dB]', fontsize=16)
